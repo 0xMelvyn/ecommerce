cf8ff3bf5d8e0bc9e13e902a34b17b9c95d98377
Melvyn <103742613+0xMelvyn@users.noreply.github.com>
2023-06-19 11:20:38 +0800

    Add files via upload
    JS Image Selector Update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function changeImage() {
-    image.src = "images/image-product-2-thumbnail.jpg";
-}
+// Attendre que le DOM soit chargé
+document.addEventListener('DOMContentLoaded', function() {
+    // Récupérer toutes les images miniatures
+    const thumbnails = document.querySelectorAll('.thumbnails img');
+  
+    // Récupérer l'image principale
+    const mainImage = document.querySelector('.product img');
+  
+    // Parcourir les images miniatures et ajouter un gestionnaire d'événement de clic à chacune
+    thumbnails.forEach(function(thumbnail) {
+      thumbnail.addEventListener('click', function() {
+        // Mettre à jour l'attribut src de l'image principale avec l'URL de l'image miniature cliquée
+        const thumbnailSrc = thumbnail.getAttribute('src');
+        mainImage.setAttribute('src', thumbnailSrc);
+      });
+    });
+  });
